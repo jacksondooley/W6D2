143ae51e302eb83ab784d51c78c430ce2978afa9
App Academy Student <email@email.com>
2022-03-08 17:01:46 -0500

    pain

--- byoar/lib/00_attr_accessor_object.rb
+++ byoar/lib/00_attr_accessor_object.rb
@@ -1,5 +1,19 @@
+require 'byebug'
+
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+        define_method("#{name}") { self.instance_variable_get("@#{name}")}
+        define_method("#{name}=") { |blank| self.instance_variable_set("@#{name}", blank)}
+    end
+    # debugger
+    # i = 0
+    # while i < names.length
+    #   name = names[i]
+    #     if name[-1] == '='
+          # define_method("#{name}") { self.instance_variable_set("@#{name}", names[i + 1])}
+    #       i += 2
+    #     end
+    # end
   end
 end
